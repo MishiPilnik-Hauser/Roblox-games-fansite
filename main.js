5ced697ce2476d3292fdd2ea864400458e68bce2
MishiPilnik-Hauser <87544308+MishiPilnik-Hauser@users.noreply.github.com>
2021-07-16 15:18:43 -0400

    Add files via upload
    3 files added.

---
+++ main.js
@@ -0,0 +1,14 @@
+function show() {
+    document.querySelector(".Gamedescription p").className="show";
+  }
+  function hide() {
+    document.querySelector(".Gamedescription p").className="hidden";
+  }
+  
+  function show2() {
+    document.getElementById("description2").className="show";
+    
+  }
+  function hide2() {
+    document.getElementById("description2").className="hidden";
+  }
